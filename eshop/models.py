13d50882c26a76edd8cd6616fa6d436cf3d25311
Chris-Erhij <Christianerhijotah@gmail.com>
2023-03-21 15:55:41 +0000

    Created; category and product models

---
+++ eshop/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.db.models import (
+    Model, CharField, SlugField, ForeignKey, ImageField, TextField, DecimalField, BooleanField,
+    DateTimeField,
+    )
+
+
+class Category(Model):
+    name: CharField = models.CharField(max_length=200)
+    slug: SlugField = models.SlugField(max_length=200, unique=True)
+
+    # Meta class for model, for ordering and indexing.
+    class Meta:
+        ordering = ['name']
+        indexes =  [
+            models.Index(fields=['name'])
+        ]
+        verbose_name: str = 'category'
+        verbose_name_plural: str = 'categories'
+
+    def __str__(self) -> str | CharField:
+        return self.name
+    
+
+class Product(Model):
+    category: ForeignKey = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name: CharField = models.CharField(max_length=200)
+    slug: SlugField = models.SlugField(max_length=200)
+    Image: ImageField = models.ImageField(upload_to='products/%y/%m/%d', blank=True)
+    description: TextField = models.TextField(blank=True)
+    price: DecimalField = models.DecimalField(max_digits=10, decimal_places=2)
+    available: BooleanField = models.BooleanField(default=True)
+    created: DateTimeField = models.DateTimeField(auto_now_add=True)
+    updated: DateTimeField = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self) -> str | CharField:
+        return self.name
