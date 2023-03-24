919bb2386cc89890b92cf089bee53920991bf8d7
Chris-Erhij <Christianerhijotah@gmail.com>
2023-03-24 11:33:10 +0000

    Correction from (local PC) on cart_remove view

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from eshop.models import Product
+from cart.forms import CartAddProductForm
+from cart import Cart
+from django.views import decorators
+from django.http import (
+    HttpRequest, HttpResponseRedirect, HttpResponsePermanentRedirect, HttpResponse
+)
+from django import forms
 
-# Create your views here.
+
+@decorators.http.require_POST
+def add_to_cart_view(request: HttpRequest, product_id: str) -> (HttpResponseRedirect | HttpResponsePermanentRedirect):
+    cart = Cart(request)  # Upon request create an empty form instance.
+    product: Product = get_object_or_404(Product, id=product_id)
+    form: (forms.Form | CartAddProductForm) = CartAddProductForm(request.POST)
+    
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
+    return redirect('cart:cart_detail_view')
+
+@decorators.http.require_POST
+def remove_from_cart_view(request: HttpRequest, product_id: str) -> (HttpResponseRedirect | HttpResponsePermanentRedirect):
+    cart = Cart(request)
+    product: Product = get_object_or_404(Product, id=product_id)
+    if product in cart:
+        cart.remove(product=product)
+    return redirect('cart:cart_detail_view')
