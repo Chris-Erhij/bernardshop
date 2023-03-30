e0f54691f4b2b61b8a20c18d7615a4ba9a5f02b2
Chris-Erhij <Christianerhijotah@gmail.com>
2023-03-30 13:04:45 +0000

    Registered orders models in a given style

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order, OrderItem
+from typing import Type
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields: list([str, ...]) = ['product']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display: list([str, ...]) = [
+        'id', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'paid', 
+        'created', 'updated'
+    ]
+
+    list_filter: list([str, ...]) = [
+        'paid', 'created', 'updated'
+    ]
+    inlines: list([Type]) = [OrderItemInline]
